7c114276c22c4bb84203ca2e049cbbd7f56ae3ff
Yuri-stack <yurioliveirasilva1999@gmail.com>
2024-09-10 16:05:15 -0300

    Aula 2 - Concluída

---
+++ web/src/app.tsx
@@ -0,0 +1,16 @@
+import { Summary } from './components/summary'
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/create-goal'
+// import { EmptyGoals } from './components/empty-goals'
+
+export function App() {
+  return (
+    <Dialog>
+      {/* <EmptyGoals /> */}
+      <Summary />
+      <CreateGoal />
+    </Dialog>
+  )
+}
+
+
